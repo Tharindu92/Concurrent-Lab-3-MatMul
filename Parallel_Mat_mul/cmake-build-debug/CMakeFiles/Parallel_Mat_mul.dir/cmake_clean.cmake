66389f397c4ea18f525d723967e4cda19bbd1413
tharindu92 <tharindu.12@cse.mrt.ac.lk>
2017-03-23 23:34:25 +0530

    Parallel Mat Mul

---
+++ Parallel_Mat_mul/cmake-build-debug/CMakeFiles/Parallel_Mat_mul.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Parallel_Mat_mul.dir/parallel_mat_mul.cpp.o"
+  "Parallel_Mat_mul.pdb"
+  "Parallel_Mat_mul"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Parallel_Mat_mul.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Parallel_Mat_mul/parallel_mat_mul.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <omp.h>
+#include <stdlib.h>
+using namespace std;
+
+double *matrixMul_double_parallel(double* matrix1, double* matrix2, int size);
+
+double* matrix1;
+double* matrix2;
+double* ans;
+int thread_count = 4;
+
+int main() {
+    int i,j,size,t_size;
+    clock_t begin, end;
+    double time_spent;
+    srand(time(NULL));
+    for(i = 0; i<10 ; i++){
+        size = 100 + i*100;
+        t_size = size * size;
+        matrix1 = (double*) malloc(t_size * sizeof(double));
+        matrix2 = (double*) malloc(t_size * sizeof(double));
+        for(j = 0; j < t_size; j++){
+            *(matrix1 + j) = random();
+            *(matrix2 + j) = random();
+        }
+        begin = clock();
+        ans = matrixMul_double_parallel(matrix1,matrix2,size);
+        end = clock();
+        time_spent = (double)(end - begin)/ CLOCKS_PER_SEC;
+        cout << "Time spent = " <<time_spent << "\nSize = " << size <<endl;
+
+    }
+
+    return 0;
+}
+
+double *matrixMul_double_parallel(double* matrix1, double* matrix2, int size){
+    double sum = 0.0;
+    int i,j,k;
+    double* ans = (double*) malloc(size * size * sizeof(double));
+    #pragma omp parallel num_threads(thread_count) private(i,j,k) shared(matrix1, matrix2, ans)
+    {
+        #pragma omp for schedule(static) reduction(+:sum)
+        for(i=0; i < size; i++){
+            for(j=0; j < size; j++){
+                sum = 0;
+                for(k=0; k< size; k++){
+                    sum += (*(matrix1+(i*size+k))) * (*(matrix2+(k*size+j)));
+                }
+                ans[i*size+j] = sum;
+            }
+        }
+    }
+    return ans;
+}
